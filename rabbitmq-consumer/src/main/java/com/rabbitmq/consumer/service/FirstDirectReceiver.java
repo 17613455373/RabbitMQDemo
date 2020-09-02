f54972fa3baaa88d3f3072a3d45e9875d515ba8c
huitengxian <huitengxian@unicdata.com>
2020-09-02 18:31:16 +0800

    想做个聊天工具

---
+++ rabbitmq-consumer/src/main/java/com/rabbitmq/consumer/service/FirstDirectReceiver.java
@@ -0,0 +1,23 @@
+package com.rabbitmq.consumer.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: Huitengxian
+ * @time: 2020/9/2 18:09
+ */
+@Service
+public class FirstDirectReceiver {
+
+    @RabbitHandler
+    @RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
+    public void process(Map testMessage) {
+        System.out.println("第一个DirectReceiver消费者收到消息  : " + testMessage.toString());
+    }
+
+}
